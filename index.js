960e2dc6d7a178fee657bf8611eaf7b066ff7ac8
Anri Kawahara <anri0806@hotmail.co.jp>
2021-03-28 19:48:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyCustomHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  `It's ${days} days until ${holiday}!`
+  `It/'s ${days} days until ${holiday}!`
 }
